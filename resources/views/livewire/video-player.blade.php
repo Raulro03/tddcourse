ff47e6bd7d997c01245fcc8657f80eebd08d274e
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-01-08 15:43:38 +0100

    Fin Paso 20

--- resources/views/livewire/video-player.blade.php
+++ resources/views/livewire/video-player.blade.php
@@ -5,3 +5,12 @@
     <h3>{{ $video->title }} ({{ $video->getReadableDuration() }})</h3>
     <p>{{ $video->description }}</p>
 </div>
+<ul>
+    @foreach($courseVideos as $courseVideo)
+        <li>
+            <a href="{{ route('pages.course-videos', $courseVideo) }}">
+                {{ $courseVideo->title }}
+            </a>
+        </li>
+    @endforeach
+</ul>
