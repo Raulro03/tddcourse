2e2da7185b6580d44d27384a7c1a826de4b6f3d1
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-01-14 16:42:14 +0100

    Fin Paso 27

--- tests/Feature/DatabaseSeederTest.php
+++ tests/Feature/DatabaseSeederTest.php
@@ -2,6 +2,8 @@
 
 
 use App\Models\Course;
+use App\Models\User;
+use App\Models\Video;
 
 it('adds given courses', function () {
     // Assert
@@ -27,15 +29,66 @@ it('adds given courses only once', function () {
 });
 
 it('adds given videos', function () {
-    // Arrange
+    // Assert
+    $this->assertDatabaseCount(Video::class, 0);
 
-    // Act & Assert
+    // Act
+    $this->artisan('db:seed');
+    $laravelForBeginners = Course::where('title', 'Laravel For Beginners')->firstOrFail();
+    $advancedLaravel = Course::where('title', 'Advanced Laravel')->firstOrFail();
+    $tddTheLaravelWay = Course::where('title', 'TDD The Laravel Way')->firstOrFail();
 
+    // Assert
+    $this->assertDatabaseCount(Video::class, 8);
+    expect($laravelForBeginners)
+        ->videos
+        ->toHaveCount(3);
+
+    expect($advancedLaravel)
+        ->videos
+        ->toHaveCount(3);
+
+    expect($tddTheLaravelWay)
+        ->videos
+        ->toHaveCount(2);
 });
 
 it('adds given videos only once', function () {
+    // Assert
+    $this->assertDatabaseCount(Video::class, 0);
+
+    // Act
+    $this->artisan('db:seed');
+    $this->artisan('db:seed');
+
+    // Assert
+    $this->assertDatabaseCount(Video::class, 8);
+});
+
+it('adds local test user', function () {
     // Arrange
+    App::partialMock()->shouldReceive('environment')->andReturn('local');
 
-    // Act & Assert
+    // Assert
+    $this->assertDatabaseCount(User::class, 0);
 
+    // Act
+    $this->artisan('db:seed');
+
+    // Assert
+    $this->assertDatabaseCount(User::class, 1);
+});
+
+it('does not test user for production', function () {
+    // Arrange
+    App::partialMock()->shouldReceive('environment')->andReturn('production');
+
+    // Assert
+    $this->assertDatabaseCount(User::class, 0);
+
+    // Act
+    $this->artisan('db:seed');
+
+    // Assert
+    $this->assertDatabaseCount(User::class, 0);
 });
