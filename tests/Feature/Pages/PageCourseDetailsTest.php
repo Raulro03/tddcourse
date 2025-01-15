a9cb91ada8bfb4f1feac0f5bfcc60f2f22a70113
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-01-15 16:47:03 +0100

    Fin Paso 30

--- tests/Feature/Pages/PageCourseDetailsTest.php
+++ tests/Feature/Pages/PageCourseDetailsTest.php
@@ -42,3 +42,19 @@ it('shows course video count', function () {
         ->assertOk()
         ->assertSeeText('3 videos');
 });
+
+it('includes paddle checkout button', function () {
+    // Arrange
+    $course = Course::factory()
+                    ->released()
+                    ->create([
+                        'paddle_product_id' => 'product-id',
+                    ]);
+
+    // Act & Assert
+    get(route('pages.course-details', $course))
+        ->assertOk()
+        ->assertSee('<script src="https://cdn.paddle.com/paddle/paddle.js"></script>', false)
+        ->assertSee('Paddle.Setup({ vendor: vendor-id });', false)
+        ->assertSee('<a href="#!" class="paddle_button" data-product="product-id">Buy Now</a>', false);
+});
