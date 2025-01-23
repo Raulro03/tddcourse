7a13b0a2676c1e829c1b47f76a621aebd9d22367
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-01-23 16:43:14 +0100

    Fin Paso 39

---
+++ tests/Feature/TweetAboutCourseReleaseCommandTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use App\Console\Commands\TwitterAboutCourseReleaseCommand;
+use App\Models\Course;
+
+it('tweets about release for provided course', function () {
+    // Arrange
+    Twitter::fake();
+    $course = Course::factory()->create();
+
+    // Act
+    $this->artisan(TwitterAboutCourseReleaseCommand::class, ['courseId' => $course->id]);
+
+    // Assert
+    Twitter::assertTweetSent("I just released {$course->title}! Check it out on: " . route('pages.course-details', $course));
+});
